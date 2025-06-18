7ea9853f3ed689e7ab72ebf6968048e4bdfa6329
hiho216 <hiho.wippo@gmail.com>
2025-06-18 23:46:02 +0700

    delete

--- page/manualbook/index.php
+++
@@ -1,9 +0,0 @@
-
-<style type="text/css">
-#myiframe {width:100%; height:1000px;} 
-</style>
-
-<body>
-<div id="scroller">
-<iframe name="myiframe" id="myiframe" src="page/manualbook/manual_book.pdf">
-</div>
